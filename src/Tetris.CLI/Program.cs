a77ee1e0773a03fc0e43f4703a0544cbd699ce84
Daniel James Dolor <estheryahe@gmail.com>
2023-01-21 10:33:36 +0800

    making working flag checking board

---
+++ src/Tetris.CLI/Program.cs
@@ -0,0 +1,21 @@
+﻿using Tetris.Core;
+
+public class Program
+{
+    public static void Main()
+    {
+        var board = new Board2D();
+        
+        board[0,0] = true;
+        board[0,1] = true;
+        board[0,2] = true;
+        board[1,2] = true;
+
+        board.printBoard();
+
+        board[1,2] = false;
+        board[0,1] = false;
+
+        board.printBoard();
+    }    
+}
---
+++ src/Tetris.Core/Board2D.cs
@@ -0,0 +1,65 @@
+﻿namespace Tetris.Core;
+
+public class Board2D
+{
+    private const byte MAX_HORIZONTAL = 10;
+    private const byte MAX_VERTICAL = 24;
+    private static void validateCoords(int vertical, int horizontal)
+    {
+        if(!(vertical >= 0 && vertical < MAX_VERTICAL))
+        {
+            throw new IndexOutOfRangeException("x is out of range");
+        }
+
+        if(!(horizontal >= 0 && horizontal < MAX_HORIZONTAL))
+        {
+            throw new IndexOutOfRangeException("y is out of range");
+        }
+    }    
+
+    private int[] _boardFlags;
+
+    public bool this[int vertical, int horizontal] 
+    {
+        get {
+            validateCoords(vertical, horizontal);
+
+            var xFlagCheck = 0b0000_0000_0000_0000_0000_0000_0000_0001 << vertical;
+            return (_boardFlags[horizontal] & xFlagCheck) > 0;
+        }
+        set {
+            validateCoords(vertical, horizontal);
+            
+            var xFlagCheck = 0b0000_0000_0000_0000_0000_0000_0000_0001 << vertical;
+            
+            if(value)
+            {
+                // A = A | B
+                _boardFlags[horizontal] |= xFlagCheck;
+            }
+            else
+            {
+                // A = A & (~A | ~B)
+                _boardFlags[horizontal] &= (~_boardFlags[horizontal] | ~xFlagCheck); 
+            }
+        }
+    }
+
+    public Board2D()
+    {
+        _boardFlags = new int[MAX_HORIZONTAL];
+    }
+    
+    public void printBoard()
+    {
+        for(int x = MAX_VERTICAL-1; x >= 0; x--)
+        {
+            Console.Write("  ");
+            for(int y = 0; y < MAX_HORIZONTAL; y++)
+            {
+                Console.Write(this[x,y] ? "██" : "░░");
+            }
+            Console.WriteLine("  ");
+        }
+    }
+}
