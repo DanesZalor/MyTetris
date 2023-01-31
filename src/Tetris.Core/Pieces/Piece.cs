659548c7f54cc770b51a6fa5662e34a55a0ce4c7
Daniel James Dolor <estheryahe@gmail.com>
2023-01-31 19:35:58 +0800

    a

--- src/Tetris.Core/Pieces/Piece.cs
+++ src/Tetris.Core/Pieces/Piece.cs
@@ -5,6 +5,19 @@ public abstract class Piece
     private Vector2D[] _blocks;
     protected Vector2D[] Blocks => _blocks;
 
+    public Vector2D Origin {
+        get 
+        {
+            Vector2D sum = new Vector2D(0,0);
+            foreach(var b in Blocks) sum += b;
+
+            return new Vector2D
+            (
+                (byte)(sum.X / Blocks.Length), 
+                (byte)(sum.Y / Blocks.Length));
+        }
+    }
+
     public Piece(Vector2D a, Vector2D b, Vector2D c, Vector2D d)
     {
         var coords = new Vector2D[]{ a, b, c, d };
@@ -19,10 +32,7 @@ public abstract class Piece
 
     public void Move(Vector2D v)
     {
-        for(int i = 0; i<4; i++)
-        {
-            Blocks[i] += v;
-        }
+        for(int i = 0; i<4; i++) Blocks[i] += v;
     }
 
     public abstract void RotateClockwise();
